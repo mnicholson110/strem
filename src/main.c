f8aa705060e0898a6012a6460c170c2925b7ee57
matt <mnicholson110@gmail.com>
2025-03-20 21:55:23 -0500

    multithreaded consumer

--- src/main.c
+++ src/main.c
@@ -3,8 +3,6 @@
 #include "../include/json_parser.h"
 #include "../include/output.h"
 
-void initOutputTypesAndFilters(strem_config_t *config, json_object *root_obj);
-
 volatile sig_atomic_t run = true;
 
 void sigterm_handle(int sig)
@@ -12,123 +10,59 @@ void sigterm_handle(int sig)
     run = false;
 }
 
+pthread_rwlock_t state_lock = PTHREAD_RWLOCK_INITIALIZER;
+
+typedef struct thread_args
+{
+    strem_config_t *config;
+    kafka_output_t *output;
+    accumulator_t **state;
+} thread_args_t;
+
+void initOutputTypesAndFilters(strem_config_t *config, json_object *root_obj);
+void *consumerThread(void *arg);
+
 int main()
 {
     signal(SIGINT, sigterm_handle);
     signal(SIGTERM, sigterm_handle);
 
     strem_config_t *config = initConfig();
-
-    kafka_input_t *input = initKafkaInput(config);
     kafka_output_t *output = initKafkaOutput();
-
-    char *message = NULL;
     accumulator_t *state = NULL;
-    accumulator_t *entry = NULL;
-    const char *out_message = NULL;
-    bool first_message = true;
-    bool filter = false;
 
-    while (run)
+    const int num_threads = 3;
+    pthread_t threads[num_threads];
+    thread_args_t args = {
+        .config = config,
+        .output = output,
+        .state = &state};
+
+    for (int i = 0; i < num_threads; ++i)
     {
-        message = pollMessage(input);
-
-        if (!message)
+        if (pthread_create(&threads[i], NULL, consumerThread, &args) != 0)
         {
-            continue;
+            fprintf(stderr, "Failed to created thread %i\n", i);
+            exit(EXIT_FAILURE);
         }
-
-        json_object *root_obj = json_tokener_parse(message);
-        if (root_obj)
-        {
-            if (first_message)
-            {
-                initOutputTypesAndFilters(config, root_obj);
-                first_message = false;
-            }
-
-            filter = applyFilter(root_obj, config);
-
-            if (filter)
-            {
-                entry = NULL;
-                const char *key = jsonGetCValue(const char *, root_obj, config->output_key);
-                HASH_FIND_STR(state, key, entry);
-                if (!entry)
-                {
-                    entry = (accumulator_t *)malloc(sizeof(accumulator_t));
-                    entry->key = key;
-                    entry->count = 1;
-                    entry->values = malloc(sizeof(accumulator_value_t) * config->input_fields_len);
-                    entry->values_len = config->input_fields_len;
-                    for (int i = 0; i < config->input_fields_len; ++i)
-                    {
-                        json_object *target = jsonGetNestedValue(root_obj, config->input_fields[i]);
-                        switch (json_object_get_type(target))
-                        {
-                        case json_type_int:
-                            entry->values[i].num = json_object_get_int(target);
-                            break;
-                        case json_type_double:
-                            entry->values[i].dub = json_object_get_double(target);
-                            break;
-                        case json_type_string:
-                            entry->values[i].str = strdup(json_object_get_string(target));
-                            break;
-                        default:
-                            break;
-                        }
-                        json_object_put(target);
-                    }
-                    HASH_ADD_STR(state, key, entry);
-                }
-                else
-                {
-                    // this should be substituted with transformation logic
-                    for (int i = 0; i < config->input_fields_len; ++i)
-                    {
-                        json_object *target = jsonGetNestedValue(root_obj, config->input_fields[i]);
-                        switch (json_object_get_type(target))
-                        {
-                        case json_type_int:
-                            entry->values[i].num += json_object_get_int(target);
-                            break;
-                        case json_type_double:
-                            entry->values[i].dub += json_object_get_double(target);
-                            break;
-                        case json_type_string:
-                            break;
-                        default:
-                            break;
-                        }
-                        json_object_put(target);
-                    }
-                    entry->count++;
-                }
-                out_message = serialize(entry, config);
-                produceMessage(output, config, entry->key, out_message);
-                free((void *)out_message);
-            }
-            json_object_put(root_obj);
-        }
-        else
-        {
-            fprintf(stderr, "No root_obj\n");
-            continue;
-        }
-        free(message);
     }
 
-    accumulator_t *tmp;
-    entry = NULL;
+    for (int i = 0; i < num_threads; ++i)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    accumulator_t *tmp = NULL;
+    accumulator_t *entry = NULL;
     HASH_ITER(hh, state, entry, tmp)
     {
         HASH_DEL(state, entry);
+        free((void *)entry->key);
+        free(entry->values);
         free(entry);
     }
 
     freeConfig(config);
-    freeKafkaInput(input);
     freeKafkaOutput(output);
 
     return 0;
@@ -188,4 +122,120 @@ void initOutputTypesAndFilters(strem_config_t *config, json_object *root_obj)
         }
         json_object_put(target);
     }
+}
+
+void *consumerThread(void *arg)
+{
+    thread_args_t *args = (thread_args_t *)arg;
+
+    kafka_input_t *input = initKafkaInput(args->config);
+    char *message = NULL;
+    accumulator_t *entry = NULL;
+    const char *out_message = NULL;
+    bool first_message = true;
+
+    while (run)
+    {
+        message = pollMessage(input);
+
+        if (!message)
+        {
+            continue;
+        }
+
+        json_object *root_obj = json_tokener_parse(message);
+        if (!root_obj)
+        {
+            fprintf(stderr, "Failed to parse message: %s\n", message);
+            free(message);
+            continue;
+        }
+
+        if (first_message)
+        {
+            pthread_rwlock_wrlock(&state_lock);
+            if (!args->config->output_types)
+            {
+                initOutputTypesAndFilters(args->config, root_obj);
+            }
+            pthread_rwlock_unlock(&state_lock);
+            first_message = false;
+        }
+
+        if (applyFilter(root_obj, args->config))
+        {
+            const char *key = jsonGetStringValue(root_obj, args->config->output_key);
+            if (!key)
+            {
+                continue;
+            }
+
+            pthread_rwlock_wrlock(&state_lock);
+            HASH_FIND_STR(*args->state, key, entry);
+            if (!entry)
+            {
+                entry = (accumulator_t *)malloc(sizeof(accumulator_t));
+                entry->key = key;
+                entry->count = 1;
+                entry->values = malloc(sizeof(accumulator_value_t) * args->config->input_fields_len);
+                entry->values_len = args->config->input_fields_len;
+                for (int i = 0; i < args->config->input_fields_len; ++i)
+                {
+                    json_object *target = jsonGetNestedValue(root_obj, args->config->input_fields[i]);
+                    switch (json_object_get_type(target))
+                    {
+                    case json_type_int:
+                        entry->values[i].num = json_object_get_int(target);
+                        break;
+                    case json_type_double:
+                        entry->values[i].dub = json_object_get_double(target);
+                        break;
+                    case json_type_string:
+                        entry->values[i].str = strdup(json_object_get_string(target));
+                        break;
+                    default:
+                        break;
+                    }
+                    json_object_put(target);
+                }
+                HASH_ADD_STR(*args->state, key, entry);
+            }
+            else
+            {
+                // pthread_mutex_lock(&entry->lock);
+                // this should be substituted with transformation logic
+                for (int i = 0; i < args->config->input_fields_len; ++i)
+                {
+                    json_object *target = jsonGetNestedValue(root_obj, args->config->input_fields[i]);
+                    switch (json_object_get_type(target))
+                    {
+                    case json_type_int:
+                        entry->values[i].num += json_object_get_int(target);
+                        break;
+                    case json_type_double:
+                        entry->values[i].dub += json_object_get_double(target);
+                        break;
+                    case json_type_string:
+                        break;
+                    default:
+                        break;
+                    }
+                    json_object_put(target);
+                }
+                entry->count++;
+                // pthread_mutex_unlock(&entry->lock);
+            }
+            pthread_rwlock_unlock(&state_lock);
+
+            pthread_mutex_lock(&entry->lock);
+            out_message = serialize(entry, args->config);
+            pthread_mutex_unlock(&entry->lock);
+            produceMessage(args->output, args->config, entry->key, out_message);
+            free((void *)out_message);
+        }
+        json_object_put(root_obj);
+        free(message);
+    }
+    freeKafkaInput(input);
+    return NULL;
 }
